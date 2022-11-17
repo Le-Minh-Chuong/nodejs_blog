6532aa72f3755959aba6452c375580a9758426fe
Minh Chuong <ch31052k@gmail.com>
2022-11-18 00:41:15 +0700

    routes and controllers

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
+const route = require('./routes');
 
 const app = express();
 const port = 3000;
@@ -9,7 +10,14 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, 'public')))
 
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
+
+// middleware
+app.use(express.urlencoded({
+    extended: true
+})); // middleware cho form html
+app.use(express.json()); // middleware gửi từ code js
+// urlencoded() và json() ở trên được lấy từ thư viện body-parser - đã đc tích hợp sẵn vào express từ phiên bản 4.16
 
 // Template engine
 app.engine('.hbs', handlebars.engine({
@@ -18,12 +26,22 @@ app.engine('.hbs', handlebars.engine({
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-})
-app.get('/news', (req, res) => {
-    res.render('news');
-})
+// app.get('/', (req, res) => {
+//     res.render('home');
+// })
+// app.get('/news', (req, res) => {
+//     res.render('news');
+// })
+// app.get('/search', (req, res) => {
+//     res.render('search');
+// })
+// app.post('/search', (req, res) => {
+//     console.log(req.body);
+//     res.send('');
+// })
+
+// routes init
+route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening at port ${port}`);
