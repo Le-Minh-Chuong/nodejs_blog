a07be058f0bf1bcf84adbe247b74755fb2e4ac79
Minh Chương <ch31052k@gmail.com>
2023-01-02 09:16:15 +0000

    Update index.js

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/education_dev');
-        console.log('Connect successfully!!!');
+        await mongoose.connect("mongodb://127.0.0.1:27017/education_dev");
+        console.log("Connect successfully!!!");
     } catch (error) {
-        console.log('Connect failure!!!. ' + error);
+        console.log("Connect failure!!!. " + error);
     }
 }
 
-module.exports = { connect }
+module.exports = { connect };
